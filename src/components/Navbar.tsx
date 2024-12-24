9dbf22ffdaa9e91736f63f5d4b179a222c94ba3d
Srini <srinismart2@gmail.com>
2024-12-24 14:32:13 +0530

    logout added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,43 +1,55 @@
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const user = useSelector((state: any) => state.user);
+
+  const logoutHandler = async (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3000/logout", {
+      method: "POST",
+      credentials: "include",
+    });
+    const data = await response.json();
+    if (data) window.location.href = "/login";
+  };
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">DevTinder 👨🏻‍💻</a>
       </div>
-      <div className="flex-none gap-2 mx-3">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="avatar"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
+      {user?.photoUrl && (
+        <div className="flex-none gap-2 mx-3">
+          <p className="text-lg">Welcome, {user?.firstName}</p>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="avatar" src={user?.photoUrl} />
+              </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a onClick={logoutHandler}>Logout</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
         </div>
-      </div>
+      )}
     </div>
   );
 };
